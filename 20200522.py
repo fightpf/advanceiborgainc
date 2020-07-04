56d26a28176499e13a8cf43063d6abb9ad56f16c
fightpf <32415029+fightpf@users.noreply.github.com>
2020-07-05 01:37:37 +0800

    Update 20200522.py

--- 20200522.py
+++ 20200522.py
@@ -24,6 +24,19 @@ for num in range(2,target_num):
         count+=1
     else:
         print(num)
+        
+        
+#埃拉托色尼法
+def eratosthenes(n):
+    IsPrime = [True] * (n + 1)
+    for i in range(2, int(n ** 0.5) + 1):
+        if IsPrime[i]:
+            for j in range(i * i, n + 1, i):
+                IsPrime[j] = False
+    return [x for x in range(2, n + 1) if IsPrime[x]]
+
+if __name__ == "__main__":
+    print(eratosthenes(120))
 #使用遞迴列印1~100
 def countingnum(numb=0):
     print(numb)
