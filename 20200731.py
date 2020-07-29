0ed0e4e62f68d1f5fce0cf7d252639c12c362a58
fightpf <32415029+fightpf@users.noreply.github.com>
2020-07-30 04:21:00 +0800

    Create 20200731.py

---
+++ 20200731.py
@@ -0,0 +1,15 @@
+a=[1,2,3,4,5,6,7]
+print(sum(a))
+
+
+class math:
+    def __init__(self,a):
+        self.a=a
+    def summ(self):
+        summ=0
+        for i in self.a
+            summ+=i
+        return summ
+
+B=math(a)
+print(B.summ())
